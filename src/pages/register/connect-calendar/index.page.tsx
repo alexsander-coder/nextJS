6862c9b8a8cfa143dc73e46be25963bff821e7a1
Alexsander Hugo Gonçalves de Souza <asouza@valeshop.com.br>
2023-11-03 10:04:53 -0300

    add time intervals

--- src/pages/register/connect-calendar/index.page.tsx
+++ src/pages/register/connect-calendar/index.page.tsx
@@ -10,7 +10,7 @@ import { useRouter } from "next/router";
 
 
 
-export default function Register() {
+export default function ConnectCalendar() {
   const session = useSession()
   const router = useRouter();
 
